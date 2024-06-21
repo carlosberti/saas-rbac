cc407739a91746f5c0a2cf697db8122c80f8b2d7
carlosberti <carlosberti182@gmail.com>
2024-06-21 14:01:32 -0300

    Add permissions and do some tests

--- packages/auth/src/permissions.ts
+++ packages/auth/src/permissions.ts
@@ -9,11 +9,18 @@ type PermissionsByRole = (
 ) => void
 
 export const permissions: Record<Role, PermissionsByRole> = {
-  ADMIN: (_, { can }) => {
+  ADMIN: (user, { can, cannot }) => {
     can('manage', 'all')
+
+    cannot(['transfer_ownership', 'update'], 'Organization')
+    can(['transfer_ownership', 'update'], 'Organization', {
+      ownerId: {
+        $eq: user.id,
+      },
+    })
   },
   MEMBER: (user, { can }) => {
-    // can('invite', 'User')
+    can('get', 'User')
     can(['create', 'get'], 'Project')
     can(['update', 'delete'], 'Project', {
       ownerId: {
@@ -21,5 +28,7 @@ export const permissions: Record<Role, PermissionsByRole> = {
       },
     })
   },
-  BILLING: () => {},
+  BILLING: (_, { can }) => {
+    can('manage', 'Billing')
+  },
 }
