da5f6ca4804095c1d7a3973e0441e10c0d343bbe
carlosberti <carlosberti182@gmail.com>
2024-06-21 13:51:02 -0300

    Add types using zod to casl permissions

--- packages/auth/src/permissions.ts
+++ packages/auth/src/permissions.ts
@@ -2,8 +2,7 @@ import { AbilityBuilder } from '@casl/ability'
 
 import { AppAbility } from '.'
 import { User } from './models/user'
-
-type Role = 'ADMIN' | 'MEMBER'
+import { Role } from './roles'
 type PermissionsByRole = (
   user: User,
   builder: AbilityBuilder<AppAbility>,
@@ -13,8 +12,14 @@ export const permissions: Record<Role, PermissionsByRole> = {
   ADMIN: (_, { can }) => {
     can('manage', 'all')
   },
-  MEMBER: (_, { can }) => {
-    can('invite', 'User')
-    can('manage', 'Project')
+  MEMBER: (user, { can }) => {
+    // can('invite', 'User')
+    can(['create', 'get'], 'Project')
+    can(['update', 'delete'], 'Project', {
+      ownerId: {
+        $eq: user.id,
+      },
+    })
   },
+  BILLING: () => {},
 }
