67255ce36b204b738dc4d88b958dc6a8cc200e04
carlosberti <carlosberti182@gmail.com>
2024-07-03 18:27:01 -0300

    get auth token before requests

--- apps/web/src/auth/auth.ts
+++ apps/web/src/auth/auth.ts
@@ -1,5 +1,24 @@
 import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
+
+import { getProfile } from '@/http/get-profile'
 
 export function isAuthenticated() {
   return cookies().has('token')
+}
+
+export async function auth() {
+  const token = cookies().has('token')
+
+  if (!token) {
+    redirect('/auth/sign-in')
+  }
+
+  try {
+    const { user } = await getProfile()
+
+    return { user }
+  } catch {}
+
+  redirect('api/auth/sign-out')
 }
