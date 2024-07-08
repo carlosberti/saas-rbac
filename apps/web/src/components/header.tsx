5d31d6c5535cc1b1e0f02da3b76fb9dc70ae5cf2
carlosberti <carlosberti182@gmail.com>
2024-07-07 21:08:52 -0300

    create middleware to get current organization and add it to organizaiton-switcher

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -9,7 +9,7 @@ import { ProfileButton } from './profile-button'
 export function Header() {
   return (
     <div className="mx-auto flex max-w-[1200px] items-center justify-between">
-      <div className="iterms-center flex gap-3">
+      <div className="flex items-center gap-3">
         <Image src={logoIcon} alt="logo" className="size-6 dark:invert" />
 
         <Slash className="size-3 -rotate-[24deg] text-border" />
